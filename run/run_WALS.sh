629f921ae0cbd14f80207315bc03dead096ae4ef
Tao Meng <tmeng@cs.ucla.edu>
2019-09-06 15:48:12 -0700

    Update run_WALS.sh

--- run/run_WALS.sh
+++ run/run_WALS.sh
@@ -11,7 +11,7 @@ cur_lang=$1
 CUDA_VISIBLE_DEVICES=7 python ../src/examples/analyze.py --parser biaffine --ordered --gpu \
 --punctuation 'PUNCT' 'SYM' --out_filename analyzer.$cur_lang.out --model_name 'network.pt' \
 --test "../data2.2/${cur_lang}_test.conllu" --model_path "./model/final_gtrans.sh_1/" --extra_embed "../data2.2/wiki.multi.${cur_lang}.vec" \
---decode proj --constraints_method PR --constraint_file "WALS_extra.pkl" --ratio_file "WALS" --mt_log "./log_he/${cur_lang}.log" --tolerance 0.01 \
+--decode proj --constraints_method PR --constraint_file "WALS_extra.pkl" --ratio_file "WALS" --mt_log "./log_he/${cur_lang}.log" \
 --summary_log 'summary.log' --gamma 1
 
 }
