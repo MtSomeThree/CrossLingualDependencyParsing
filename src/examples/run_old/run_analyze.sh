aec5e930ffa4e0f05be3440dccc30824040a0a5e
Tao Meng <tmeng@cs.ucla.edu>
2019-09-06 15:51:04 -0700

    update

--- src/examples/run_old/run_analyze.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=$1 python2 examples/analyze.py --parser stackptr --beam 5 --ordered --gpu \
- --punctuation 'PUNCT' 'SYM' --out_filename analyzer_out \
- --test "data/udt_google/German/de-ud-test.upos.conll" \
- --model_path "models/parsing/stack_ptr_en_de_test/" --model_name 'network.pt'
