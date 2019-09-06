3a65cf3a2664930d85d8ac563ccd92b088e39f31
Tao Meng <tmeng@cs.ucla.edu>
2019-09-05 17:50:19 -0700

    Update run_LR.sh

--- run/run_LR.sh
+++ run/run_LR.sh
@@ -11,8 +11,8 @@ cur_lang=$1
 CUDA_VISIBLE_DEVICES=7 python ../src/examples/analyze.py --parser biaffine --ordered --gpu \
 --punctuation 'PUNCT' 'SYM' --out_filename analyzer.$cur_lang.out --model_name 'network.pt' \
 --test "../data2.2/${cur_lang}_test.conllu" --model_path "./model/final_gtrans.sh_1/" --extra_embed "../data2.2/wiki.multi.${cur_lang}.vec" \
---decode proj --constraints_method PR --constraint_file "WALS_extra.pkl" --ratio_file "WALS" --mt_log "./log_he/${cur_lang}.log" --tolerance 0.01 \
---summary_log 'summary.log' --gamma 1
+--decode proj --constraints_method Lagrange --constraint_file "./constraints.txt" --ratio_file "./model/constraints/${cur_lang}.constraint" \
+--mt_log "./log/${cur_lang}2.log"
 
 }
 
